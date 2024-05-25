00e0188d22668892500dbd87b8f18262f4f1e067
Your Name <you@example.com>
2024-05-25 20:12:50 +0530

    learn about components and added style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Card() {
+  return (
+    <div
+      style={{
+        border: "solid black 2px",
+        padding: "10px",
+        margin: "10px",
+        borderRadius: "20px",
+      }}
+    >
+      <img
+        src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?q=80&w=100&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Dp"
+        style={{ borderRadius: "50%" }}
+      />
+      <h1
+        style={{
+          color: "red",
+          fontSize: "30px",
+          marginTop: "0px",
+          marginBottom: "0px",
+        }}
+      >
+        @Bharat
+      </h1>
+      <p style={{ marginTop: "0px" }}>My Life , My Rules</p>
+      <div
+        style={{
+          backgroundColor: "blue",
+          color: "white",
+          padding: "12px",
+          borderRadius: "10px",
+          width: "50px",
+        }}
+      >
+        Follow
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
